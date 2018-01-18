a73a3176621256fe829e5bbb66188087be4fd80e
Çağıl Uluşahin <cagilulusahin@gmail.com>
2018-01-18 19:17:59 +0300

    plot_word_freqs_given_a_regexp_for_each_year function added

--- corpus_loader.py
+++ corpus_loader.py
@@ -1,13 +1,12 @@
 import configparser
-from tbmmcorpus import TbmmCorpus
+import  tbmmcorpus
 
 config_parser = configparser.ConfigParser()
 config_parser.read("config.ini")
 config = config_parser['default']
 
 
-
-corpus = TbmmCorpus(metadata=True, config=config)
+corpus = tbmmcorpus.TbmmCorpus(metadata=True, config=config)
 corpus.load_tbmm_corpus("corpus-v0.1/tbmm_corpus.mm")
 corpus.prepare_metadata_to_description_dictionary()
 corpus.generate_word_counts()
