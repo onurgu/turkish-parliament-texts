ce0e66b729243072c2e7fbf728d50181e2a86aca
Çağıl Uluşahin <cagilulusahin@gmail.com>
2018-01-20 13:41:24 +0300

    use development settings

--- corpus_loader.py
+++ corpus_loader.py
@@ -1,13 +1,17 @@
 import configparser
 import  tbmmcorpus
 
+DEV=False
+
 config_parser = configparser.ConfigParser()
 config_parser.read("config.ini")
 config = config_parser['default']
 
 
 corpus = tbmmcorpus.TbmmCorpus(metadata=True, config=config)
-corpus.load_tbmm_corpus("corpus-dev/tbmm_corpus")
-corpus.load_tbmm_corpus("corpus-v0.1/tbmm_corpus.mm")
+if DEV:
+    corpus.load_tbmm_corpus("corpus-dev/tbmm_corpus")
+else:
+    corpus.load_tbmm_corpus("corpus-v0.1/tbmm_corpus.mm")
 corpus.prepare_metadata_to_description_dictionary()
 corpus.generate_word_counts()
