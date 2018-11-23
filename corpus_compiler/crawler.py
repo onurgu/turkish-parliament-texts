b1372332cdc0fb5e7ac599bbec8239415b820f88
onur güngör <onurgu@gmail.com>
2018-11-23 23:13:55 +0300

    WIP for crawling and updating with new sessions

--- corpus_compiler/crawler.py
+++ corpus_compiler/crawler.py
@@ -1,23 +1,27 @@
 import configparser
+import datetime
 import os
 import sys
 import wget
 import re
 import requests
+import sqlite3
 import time
-
 
 config_parser = configparser.ConfigParser()
 config_parser.read("config.ini")
 config = config_parser['default']
 
-all_donem_url = 'https://www.tbmm.gov.tr/tutanak/tutanaklar.htm'
-base = 'https://www.tbmm.gov.tr/develop/owa/'
-
 
 def gather_new_html_documents():
+    file_based_storage_dir = config['file_based_storage_dir']
+    conn = sqlite3.connect(os.path.join(file_based_storage_dir, "metadb.sqlite"))
+    cursor = conn.cursor()
+    session = requests.session()
+    session.proxies = {}
+    all_donem_url = config["all_donem_url"]
     # encoding ISO-8859-1
-    all_donem_page = download_page(all_donem_url)
+    all_donem_page = download_page(session, all_donem_url)
     donem_pages = {}
 
     p = re.compile(r'"(https://www.tbmm.gov.tr/tutanak/donem[^"]*)?"')
@@ -30,10 +34,20 @@ def gather_new_html_documents():
         file_links = []
 
         p_sub = re.compile(r'"(https://www.tbmm.gov.tr/tutanak/{}/yil{}/ham/[^"]*)?"'.format(donem_name, year))
-        for m_1 in p_sub.finditer(download_page(m.group(1))):
+        for m_1 in p_sub.finditer(download_page(session, url)):
             file_link = m_1.group(1)
             if 'oylama' not in file_link:
                 file_links.append(file_link)
+
+        for file_link in file_links:
+            tutanak_content_response = session.get(file_link)
+            if tutanak_content_response.ok:
+                tutanak_content = tutanak_content_response.content.decode("iso-8859-9")
+                file_path = os.path.join(file_based_storage_dir, url.replace("https://www.tbmm.gov.tr/tutanak/", '').replace("/", "_"))
+                with open(file_path, "w") as f:
+                    f.write(tutanak_content)
+                cursor.execute(f"INSERT INTO files (file_type, file_url, referring_url, file_path, created_at) "
+                             f"VALUES ('html', '{file_link}', '{url}', '{file_path}', '{datetime.datetime.now()}')")
 
         donem_pages[url] = file_links
 
@@ -41,18 +55,14 @@ def gather_new_html_documents():
     return donem_pages
 
 
-session = requests.session()
-
-file_based_storage_dir = config['file_based_storage_dir']
-
-
 def get_last_session_id():
     return 0
 
 
-def download_page(url):
-    response = session.get(url)
-    time.sleep(0.1)
+def download_page(session, url):
+    print(url)
+    response = session.get(url, timeout=10)
+    time.sleep(3)
     return response.text
 
 
@@ -115,7 +125,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--command", default="main", choices=["download_session_pdfs", "update_session_htmls_from_web"],
+    parser.add_argument("--command", default="main", choices=["download_session_pdfs", "update_session_htmls_from_web", "init_db"],
                         required=True)
     parser.add_argument("--input_file")
 
@@ -124,5 +134,16 @@ if __name__ == "__main__":
     if args.command == "download_session_pdfs":
         download_session_pdfs()
     elif args.command == "update_session_htmls_from_web":
-        pass
+        print(gather_new_html_documents())
+    elif args.command == "init_db":
+        file_based_storage_dir = config['file_based_storage_dir']
+
+        conn = sqlite3.connect(os.path.join(file_based_storage_dir, "metadb.sqlite"))
+
+        cursor = conn.cursor()
+
+        cursor.execute("DROP TABLE files")
+        cursor.execute("CREATE TABLE files (id integer primary key, file_type text, file_url text, referring_url text, file_path text, created_at date)")
+
+
 
